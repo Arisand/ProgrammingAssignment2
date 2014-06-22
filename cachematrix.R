1c07a83f1a81f728f81fca072e36880e0b250cf2
Arisand <mauette2000@yahoo.com>
2014-06-21 22:16:57 -0400

    required submission for Prog assgn 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setx <- function(mean) m <<- x
+        getx <- function() m
+        list(set = set, get = get,
+             setx = setx,
+             getx = getx)
+        
 
 }
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getx()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setx(m)
+        m
+        
 }
